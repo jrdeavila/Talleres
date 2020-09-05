e425d40e5c9fd7be6f22258f1700809a8c320955
jrdeavila <jrdeavila@unicesar.edu.co>
2020-09-04 19:36:41 -0500

    Primer Taller

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/* Capturar 3 notas, sacar el Promedio y determinar si el estudiante ganó o perdió la asignatura (Mayor o igual a 3 ganó en
+caso contrario Perdió)*/
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] notas = new float[3];
+            for(int i=0;i<notas.Length;i++){
+                Console.Write("Nota [{0}]: ", i+1);
+                notas[i] = float.Parse(Console.ReadLine());
+            }
+            float aux = 0;
+            for(int i=0;i<notas.Length;i++){
+                aux += notas[i];
+            }
+
+            aux /= 3;
+
+            Console.Write("Su promedio es: {0}, ", aux);
+
+            if(aux >= 3){
+                Console.WriteLine("Gano la asignatura...");
+            }
+            else{
+                Console.WriteLine("Perdio la asignatura...");
+            }
+
+        }
+    }
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+/*
+Calcular el número de pulsaciones que debe tener una persona por cada 10 segundos 
+de ejercicio aeróbico; la formula que se aplica cuando el
+
+sexo es femenino es:
+pulsaciones = (220 -edad)/10
+si el sexo es masculino:
+pulsaciones = (210 -edad)/10
+*/
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba su nombre: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            string sexo;
+
+            do{
+                Console.Write("Sexo (M/F): ");
+                sexo = Console.ReadLine();
+                sexo = sexo.ToUpper();
+            }while(!"M".Equals(sexo) && !"F".Equals(sexo));
+
+            float pulse = 0;
+            if(sexo.Equals("F")){
+                pulse = 220-edad;
+                pulse /= 10;
+            }
+            else{
+                pulse = 210-edad;
+                pulse /= 10;
+            }
+
+            Console.WriteLine("{0}, usted tiene {1} pulsaciones por cada 10 segundos...", nombre, pulse);
+
+        }
+    }
+}
